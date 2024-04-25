bdb14ed74a29d4a82a2410193310340399cd7eb5
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-04-25 13:14:24 +0530

    Make the design responsive
    Add a toast message when password don't match. Fix design issues and
    make the design mobile friendly.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ window.addEventListener("DOMContentLoaded", () => {
     const passwordField = document.querySelector("#password");
     const conPasswordField = document.querySelector("#con-password");
     const submitBtn = document.querySelector("button[type='submit']");
-
+    const toast = document.querySelector(".toast-message");
     submitBtn.addEventListener("click", (event) => {
         if (passwordField.value != conPasswordField.value) {
-            alert("Passwords don't match");
+            event.preventDefault();
+            toast.classList.remove("hide");
+            setTimeout(() => {
+                toast.classList.add("hide");
+            }, 2000);
         }
     })
 })
